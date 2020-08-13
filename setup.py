4298e79916b239a83dd2cb7172d700c47a9a922a
Narikawa <38754367+TakaoNarikawa@users.noreply.github.com>
2020-08-13 18:10:53 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup, Command
+
+setup(name='resnet-attention',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='TakaoNarikawa',
+      author_email='takao.narikawa@gmail.com',
+      packages=find_packages(),
+     )
